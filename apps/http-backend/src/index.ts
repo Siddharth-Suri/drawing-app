d16f1f13a6d906f0a30ea9330ca3646653253073
Siddharth Suri <siddharth2005suri@gmail.com>
2025-07-19 15:45:23 +0530

    Added basic stuff

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,63 @@
 import express from "express"
-
+import * as jwt from "jsonwebtoken"
+import { middleware } from "./middleware.js"
+import { JWT_SECRET } from "@repo/backend-common/config"
+import {
+    CreateUserSchema,
+    CreateRoomSchema,
+    SigninSchema,
+} from "@repo/common/types"
 const app = express()
 const port = 3000
+const users: any = []
+
+app.use(express.json())
+
+app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body)
+
+    if (!data.success) {
+        return res
+            .status(404)
+            .json({ message: "Username or Password are missing" })
+    }
+
+    // db call here
+
+    res.status(200).json({
+        userid: 123,
+    })
+})
+
+app.post("/signin", (req, res) => {
+    const userId = 1
+    const data = SigninSchema.safeParse(req.body)
+
+    if (!data.success) {
+        return res
+            .status(404)
+            .json({ message: "Username or Password are missing" })
+    }
+
+    const token = jwt.sign({ userId }, JWT_SECRET)
+    res.json({
+        token,
+    })
+})
+
+app.post("/room", middleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body)
+
+    if (!data.success) {
+        return res
+            .status(404)
+            .json({ message: "Username or Password are missing" })
+    }
+
+    res.json({
+        roomId: 123,
+    })
+})
 
 app.listen(port, () => {
     console.log("Started http server: " + port)
